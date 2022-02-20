168e03fadb45b154da872d97fee1f17f4931c6f9
RitaGafni <gafnirita@gmail.com>
2022-02-20 14:05:35 +0200

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCXRkiPQLIDpvYeQ0bWIGXvflHe-af5RSY',
+  authDomain: 'my-weather-app-b31d0.firebaseapp.com',
+  projectId: 'my-weather-app-b31d0',
+  storageBucket: 'my-weather-app-b31d0.appspot.com',
+  messagingSenderId: '777962948154',
+  appId: '1:777962948154:web:c08e2990033a45ba955703',
+  measurementId: 'G-43S28K6X4V',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
